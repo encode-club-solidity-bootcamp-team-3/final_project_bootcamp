08d57e00fd7a811a4132cf7bb646d0b7a8cb9f07
JordiRuspira <34830607+JordiRuspira@users.noreply.github.com>
2023-08-31 15:58:46 +0200

    Update closeLottery.ts

--- protocol/scripts/closeLottery.ts
+++ protocol/scripts/closeLottery.ts
@@ -9,15 +9,28 @@ async function main() {
 
     // Replace with the actual contract and token addresses
     const lotteryContractAddress = '0x8fee3143154bc482305010b3cd9546c7f6f1a667';
-   
+    const nftContractAddress = '0xc6c08fc1d46103ef9c9afe1d1a690b475715577d';
+  
     // Load the contract and contract token ABIs from the JSON files
     const lotteryContractABI = require('../artifacts/contracts/Lottery.sol/Lottery.json').abi;
+    const nftContractABI = require('../artifacts/contracts/NFTContract.sol/NFTContract.json').abi;
   
     // Create instances of the contracts
     const lotteryContract = new ethers.Contract(lotteryContractAddress, lotteryContractABI, wallet);
-    
+    const nftContract = new ethers.Contract(nftContractAddress, nftContractABI, wallet);
+  
    // const approval = await lotteryTokenContract.approve(process.env.LOTTERY_ADDRESS, ethers.parseEther(String("1")));
-    const betTx = await lotteryContract.closeLottery(55);
+   
+   // Before calling closeLottery
+    const callerAddress = wallet.address;
+
+    const nftOwner = await nftContract.ownerOf(65); // Replace with the correct tokenId
+   
+    console.log(nftOwner);
+    console.log(callerAddress);
+
+   
+   const betTx = await lotteryContract.closeLottery(65);
     const receipt = await betTx.wait();
     console.log(receipt);
     return { success: true, txHash: betTx.hash };
