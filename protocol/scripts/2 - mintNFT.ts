f1d1d8c13f8236f2f8e8390f6a71777b759987f6
JordiRuspira <34830607+JordiRuspira@users.noreply.github.com>
2023-09-03 00:11:11 +0200

    Update and rename mintNFT.ts to 2 - mintNFT.ts

--- protocol/scripts/2 - mintNFT.ts
+++ protocol/scripts/2 - mintNFT.ts
@@ -1,5 +1,5 @@
 import { ethers } from "ethers";
-import { NFTContract, NFTContract__factory } from "../typechain-types"; // Import both Lottery and Lottery__factory
+import { NFTContract, NFTContract__factory } from "../typechain-types/"; // Import both Lottery and Lottery__factory
 import * as dotenv from "dotenv";
 dotenv.config();
 
@@ -11,12 +11,13 @@ function setupProvider() {
 async function main() {
     const provider = setupProvider();
     const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider);
-    
-    const nftContract = NFTContract__factory.connect("0xc6c08fc1d46103ef9c9afe1d1a690b475715577d", wallet
+
+    // Insert here NFT Contract address
+    const nftContract = NFTContract__factory.connect("0x87687a0d0ac168dc66f5819d3e0d56219b693e32", wallet
   );
     
     const recipientAddress = "0x1121A584c4CdB01753F030e0b50FAC9475b503DD"; // Replace with the recipient's address
-    const tokenId = 65; // Specify the tokenId you want to mint
+    const tokenId = 85; // Specify the tokenId you want to mint
     
     // Mint the NFT
     const mintTransaction = await nftContract.safeMint(recipientAddress, tokenId);
