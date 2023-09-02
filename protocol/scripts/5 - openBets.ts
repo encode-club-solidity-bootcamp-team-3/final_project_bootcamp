0297f752f19ef503b6e5ebfc1882c71f84fdd82f
JordiRuspira <34830607+JordiRuspira@users.noreply.github.com>
2023-09-03 00:26:39 +0200

    Update 5 - openBets.ts

--- protocol/scripts/5 - openBets.ts
+++ protocol/scripts/5 - openBets.ts
@@ -7,15 +7,15 @@ async function main() {
     const provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL ?? "");
     const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider);
 
-    // Replace with the actual contract address
-    const lotteryContractAddress = '0xc510e80F833Ed283212DE560b97097392F594323'; 
+    // Replace with the actual contract addresses
+    const lotteryContractAddress = '0xc510e80F833Ed283212DE560b97097392F594323';
     
     // Load the contract ABIs from the JSON files
     const lotteryContractABI = require('../artifacts/contracts/Lottery.sol/Lottery.json').abi;
-    
-    // Create instances of the contract
-    const lotteryContract = new ethers.Contract(lotteryContractAddress, lotteryContractABI, wallet); 
 
+    // Create instances of the contract
+    const lotteryContract = new ethers.Contract(lotteryContractAddress, lotteryContractABI, wallet);
+    
     // Get the current block timestamp
     const currentBlock = await provider.getBlock("latest");
     const timestamp = currentBlock?.timestamp ?? 0;
