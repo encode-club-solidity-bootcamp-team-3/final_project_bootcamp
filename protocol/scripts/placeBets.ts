11b4b69889d6239230069b45a810ffade68a4d0b
JordiRuspira <34830607+JordiRuspira@users.noreply.github.com>
2023-09-03 00:15:51 +0200

    Update placeBets.ts

--- protocol/scripts/placeBets.ts
+++ protocol/scripts/placeBets.ts
@@ -8,8 +8,8 @@ async function main() {
     const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider);
 
     // Replace with the actual contract and token addresses
-    const lotteryContractAddress = '0x8fee3143154bc482305010b3cd9546c7f6f1a667';
-    const lotteryTokenContractAddress = '0x64b3BC95286A87C06a6aa8FcDc92dD3b2f695496';
+    const lotteryContractAddress = '0xc510e80F833Ed283212DE560b97097392F594323';
+    const lotteryTokenContractAddress = '0x8795C5e3C00d2a464D18d60237B8b0eA16b6fD44';
  
     // Load the contract and contract token ABIs from the JSON files
     const lotteryContractABI = require('../artifacts/contracts/Lottery.sol/Lottery.json').abi;
@@ -25,8 +25,7 @@ async function main() {
       );
       await allowTx.wait();
   
-      console.log('Allowed', allowTx.hash);
-   // const approval = await lotteryTokenContract.approve(process.env.LOTTERY_ADDRESS, ethers.parseEther(String("1")));
+    console.log('Allowed', allowTx.hash);
     const betTx = await lotteryContract.bet();
     
     const receipt = await betTx.wait();
