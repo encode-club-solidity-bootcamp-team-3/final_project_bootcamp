def006de4cd9f3e8953d8112707f6ac1042058e3
JordiRuspira <34830607+JordiRuspira@users.noreply.github.com>
2023-09-04 11:52:30 +0200

    Update 4 - buyTokens.ts

--- protocol/scripts/4 - buyTokens.ts
+++ protocol/scripts/4 - buyTokens.ts
@@ -1,5 +1,5 @@
 import { ethers } from "ethers";
-import { Lottery, Lottery__factory } from "../typechain-types/";
+import { Lottery } from "../typechain-types/";
 import * as dotenv from "dotenv";
 dotenv.config();
 
@@ -7,17 +7,16 @@ async function main() {
   const provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL ?? "");
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider);
   
-  // Replace with the contract address
-  const lotteryContractAddress = "0xc510e80F833Ed283212DE560b97097392F594323";
-
+  // Replace with the actual contract address
+  const lotteryContractAddress = "0x30d29200fa4d936ddcf1d36bbfcc3a3781c685a7";
+  
   // Load the contract ABI
   const lotteryContractABI = require("../artifacts/contracts/Lottery.sol/Lottery.json").abi;
   
-  // Create an instance of the Lottery contract. 
-  const lotteryContract = new ethers.Contract(lotteryContractAddress, lotteryContractABI, wallet) as unknown as LotteryNew;
-
-  // Change this to the desired amount
-  const etherAmount = 0.03; 
+  // Create an instance of the Lottery contract
+  const lotteryContract = new ethers.Contract(lotteryContractAddress, lotteryContractABI, wallet) as unknown as Lottery;
+  
+  const etherAmount = 0.03; // Change this to the desired amount
 
   // Convert the Ether amount to wei
   const weiAmount = ethers.parseEther(etherAmount.toString());
@@ -26,7 +25,8 @@ async function main() {
   const purchaseTx = await lotteryContract.connect(wallet).purchaseTokens(weiAmount, { value: weiAmount });
 
   const purchaseReceipt = await purchaseTx.wait();
-  //console.log(`Tokens purchased: ${ethers.formatUnits(tokenAmount)} tokens`);
+
+
   console.log("Transaction receipt:", purchaseReceipt);
 }
 
